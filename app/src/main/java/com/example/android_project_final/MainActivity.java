94d745e1c37b7e699f8696ea50b17e18b8b17dda
rene777reyes <renreyes@csumb.edu>
2025-04-28 13:52:13 -0700

    Created multiple items (fonts, colors, etc) in the themes.xml so that all app pages have a consistent aesthetic throughout

--- app/src/main/java/com/example/android_project_final/MainActivity.java
+++ app/src/main/java/com/example/android_project_final/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.android_project_final;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.EditText;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
