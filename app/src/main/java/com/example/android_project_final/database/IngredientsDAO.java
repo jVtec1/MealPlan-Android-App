cbc759961de7597351da00c0ef43dccc16358ebe
Andy Espinoza <andyesp831@gmail.com>
2025-05-01 15:47:07 -0700

    Initialized files Ingredients.java and IngredientsDAO.java for database new ingredients table

---
+++ app/src/main/java/com/example/android_project_final/database/IngredientsDAO.java
@@ -0,0 +1,4 @@
+package com.example.android_project_final.database;
+
+public interface IngredientsDAO {
+}
---
+++ app/src/main/java/com/example/android_project_final/database/entities/Ingredients.java
@@ -0,0 +1,4 @@
+package com.example.android_project_final.database.entities;
+
+public class Ingredients {
+}
